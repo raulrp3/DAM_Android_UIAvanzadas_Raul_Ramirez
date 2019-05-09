1577a611a60e38517766229a07e8af0b269c1121
raulrp3 <raul.ramirez@escuelaestech.es>
2019-05-09 14:17:15 +0200

    init ui LoadWebFragment

--- app/src/main/java/com/example/alumno_fp/ui_avanzadas/Fragments/LoadWebFragment.java
+++ app/src/main/java/com/example/alumno_fp/ui_avanzadas/Fragments/LoadWebFragment.java
@@ -6,14 +6,21 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.alumno_fp.ui_avanzadas.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class LoadWebFragment extends Fragment {
+public class LoadWebFragment extends Fragment implements View.OnClickListener {
 
+    private EditText editTextUrl;
+    private Button buttonFirstWeb;
+    private Button buttonSecondWeb;
 
     public LoadWebFragment() {
         // Required empty public constructor
@@ -24,7 +31,31 @@ public class LoadWebFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_load_web, container, false);
+        View view = inflater.inflate(R.layout.fragment_load_web, container, false);
+
+        editTextUrl = view.findViewById(R.id.etUrl);
+        buttonFirstWeb = view.findViewById(R.id.btnFirstWeb);
+        buttonSecondWeb = view.findViewById(R.id.btnSecondWeb);
+
+        buttonFirstWeb.setOnClickListener(this);
+        buttonSecondWeb.setOnClickListener(this);
+
+        return view;
     }
 
+    @Override
+    public void onClick(View v) {
+        String url = editTextUrl.getText().toString().trim();
+
+        switch (v.getId()){
+            case R.id.btnFirstWeb : {
+                Toast.makeText(v.getContext(),"First web: " + url,Toast.LENGTH_SHORT).show();
+            }
+            break;
+            case R.id.btnSecondWeb: {
+                Toast.makeText(v.getContext(),"Secondn web: " + url,Toast.LENGTH_SHORT).show();
+            }
+            break;
+        }
+    }
 }
