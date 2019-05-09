2bc986e9b8c604660bdcae28bc904d650c2e2a77
raulrp3 <raul.ramirez@escuelaestech.es>
2019-05-09 14:01:42 +0200

    add webview for first web and second web

--- app/src/main/java/com/example/alumno_fp/ui_avanzadas/Fragments/FirstWebFragment.java
+++ app/src/main/java/com/example/alumno_fp/ui_avanzadas/Fragments/FirstWebFragment.java
@@ -3,9 +3,13 @@ package com.example.alumno_fp.ui_avanzadas.Fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.example.alumno_fp.ui_avanzadas.R;
 
@@ -14,6 +18,7 @@ import com.example.alumno_fp.ui_avanzadas.R;
  */
 public class FirstWebFragment extends Fragment {
 
+    private WebView webView;
 
     public FirstWebFragment() {
         // Required empty public constructor
@@ -23,8 +28,15 @@ public class FirstWebFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first_web, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_first_web, container, false);
+
+        webView = view.findViewById(R.id.webViewFirst);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+
+        return view;
     }
 
 }
--- app/src/main/java/com/example/alumno_fp/ui_avanzadas/Fragments/SecondWebFragment.java
+++ app/src/main/java/com/example/alumno_fp/ui_avanzadas/Fragments/SecondWebFragment.java
@@ -6,6 +6,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.example.alumno_fp.ui_avanzadas.R;
 
@@ -14,6 +17,7 @@ import com.example.alumno_fp.ui_avanzadas.R;
  */
 public class SecondWebFragment extends Fragment {
 
+    private WebView webView;
 
     public SecondWebFragment() {
         // Required empty public constructor
@@ -23,8 +27,15 @@ public class SecondWebFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second_web, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_second_web, container, false);
+
+        webView = view.findViewById(R.id.webViewSecond);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+
+        return view;
     }
 
 }
